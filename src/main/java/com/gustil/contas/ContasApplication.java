35d49775af9ceff5d840df1195367a443bb48de4
Matheus Toledo <matheusot.900009306@uniacademia.edu.br>
2022-06-05 16:10:34 -0300

    adicinado e configurado Discovery ao projeto

--- src/main/java/com/gustil/contas/ContasApplication.java
+++ src/main/java/com/gustil/contas/ContasApplication.java
@@ -2,6 +2,9 @@ package com.gustil.contas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class ContasApplication {
